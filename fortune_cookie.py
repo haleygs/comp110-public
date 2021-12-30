c218d093c115c04c02a943fdcb6723825c6b5df4
Haley Sears <haleygs@email.unc.edu>
2021-12-30 15:55:50 -0500

    Updated Fortune Cookie program.

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -5,19 +5,30 @@ __author__ = "haleygs@email.unc.edu"
 # Import randint function from the random library
 from random import randint
 
-print("Your fortune cookie says... ")
+def main() -> None:
+    """The entrypoint of the program, when run as a module."""
+    print("Your fortune cookie says...")
+    print(fortune_cookie())
+    print("Now, go spread positive vibes!")
+
 
 some_int: int = randint(1, 4)
 
-if some_int == 1:
-    print("You can make your own happiness.")
-else:
-    if some_int == 2:
-        print("Your ability for accomplishment will follow with success.")
-    else: 
-        if some_int == 3:
-            print("Jealousy doesn't open doors, it closes them.")
-        else:
-            print("A new voyage will fill your life with untold memories.")
 
-print("Now, go spread positive vibes!")
+def fortune_cookie() -> str:
+    """Returns a random fortune."""
+    if some_int == 1:
+        return "You can make your own happiness!"
+    else:
+        if some_int == 2:
+            return "Your ability for accomplishment will follow with success."
+        else:
+            if some_int == 3:
+                return "Jealousy doesn't open doors, it closes them!"
+            else:
+                return "A new voyage will fill your life with untold memories."
+
+
+# Special dunder variable __name__ for "starting" the program when run as a module.
+if __name__ == "__main__":
+    main()
